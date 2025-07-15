47af329d8431a64e406d3b2ec43ff5829655812a
Viktor Ninov <viktor.ninov@mail.schwarz>
2025-07-15 07:45:01 +0300

    Set upstream repo

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "null" {}
 
-resource "null_resource" "intership_greeting" {
+resource "null_resource" "intership_greeting_2025" {
   provisioner "local-exec" {
     command = "echo 'Hello students, welcome to STACKIT training 2025.'"
   }
